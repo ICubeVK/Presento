7f3b324084925e343b41f0ddbc92430287596771
iCubeVK <199650705+ICubeVK@users.noreply.github.com>
2026-09-16 22:11:40 +0200

    Completed basic dock management

--- js/dock.js
+++ js/dock.js
@@ -1,21 +1,42 @@
 let hideTimer;
 let dock = document.getElementById("dock");
-let page = document.getElementById("page");
 let preview = document.getElementById("preview");
+let dockHovered = false;
+
+function showDock() {
+    dock.classList.add("dock_active")
+    preview.classList.add("dock_active")
+
+    clearTimeout(hideTimer);
+
+    if (!dockHovered) {
+        hideTimer = setTimeout(hideDock, 2000);
+    }
+}
+
+function hideDock() {
+    if (dockHovered) return;
+    dock.classList.remove("dock_active");
+    preview.classList.remove("dock_active");
+}
 
 document.addEventListener("mousemove", (event) => {
-    // if near mouse bottom
+    // if near mouse top
     // "window.innerHeight -" for bottom
-    if (event.clientY < 25) {
-        dock.classList.add("dock_active")
-        preview.classList.add("dock_active")
-
-        clearTimeout(hideTimer);
-
-        hideTimer = setTimeout(() => {
-            dock.classList.remove("dock_active")
-            preview.classList.remove("dock_active")
-        }, 2000);
+    if (event.clientY < 15) {
+        showDock();
     }
 });
 // TODO: make the dock position selectable
+
+dock.addEventListener("mouseenter", () => {
+    dockHovered = true;
+    clearTimeout(hideTimer);
+});
+
+dock.addEventListener("mouseleave", () => {
+    dockHovered = false;
+
+    // Start the timer again once the mouse leaves
+    hideTimer = setTimeout(hideDock, 2000);
+});
